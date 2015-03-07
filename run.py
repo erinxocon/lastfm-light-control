95941c52b71384480fe7158565efd0da7d573df5
Erin O'Connell <erinocon5@gmail.com>
2015-03-07 02:12:59 -0700

    run the main logic

--- run.py
+++ run.py
@@ -2,6 +2,9 @@ import lastfm
 import logging
 import sys
 import helpers
+import open_pixel
+
+from time import sleep
 
 
 def run():
@@ -11,9 +14,20 @@ def run():
     last_creds = helpers.get_config('Last_fm')
     logging.info('Genre Colors are: '+str(genre_colors))
     network = lastfm.LastFM(last_creds['user'], last_creds['api_key'], genre_colors)
-    current = network.get_now_playing()
-    print(current.get_track_tags())
-    print(current.color())
+    old_color = 'White'
+    while True:
+        current = network.get_now_playing()
+        if current.is_playing:
+            #color = current.color()
+            #logging.info(current.get_artist_tags())
+            #logging.info(color)
+            rgb = open_pixel.create_color(current.color())
+            logging.info(rgb)
+            #open_pixel.fade_neopixel(color, old_color, 10, 0.01)
+            open_pixel.set_neopixel_color(rgb)
+            #old_color = color
+        sleep(5)
+
 
 
 if __name__ == '__main__':
